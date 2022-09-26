655a9ea21013ac1b569aed9b21e818ffbe84e548
unknown <rorchenko@gmail.com>
2022-09-26 22:31:33 +0300

    class-validator

--- src/member/member.dto.ts
+++ src/member/member.dto.ts
@@ -1,16 +1,26 @@
-export interface CreateMemberDto {
+import { IsNumber, IsString } from 'class-validator';
+
+export class CreateMemberDto {
+  @IsString()
   name: string;
+  @IsString()
   birth: string;
 }
 
-export interface UpdateMemberDto {
+export class UpdateMemberDto {
+  @IsNumber()
   id: number;
+  @IsString()
   name: string;
+  @IsString()
   birth: string;
 }
 
-export interface CreateChildMemberDto {
+export class CreateChildMemberDto {
+  @IsNumber()
   parentId: number;
+  @IsString()
   name: string;
+  @IsString()
   birth: string;
 }
